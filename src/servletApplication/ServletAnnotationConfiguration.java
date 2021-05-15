e19658b2e09319cc6b6157782a6531311b22689a
ankan03 <1828049@kiit.ac.in>
2021-05-15 21:27:19 +0530

    Little change in ServletAnnotationConfiguration

--- src/servletApplication/ServletAnnotationConfiguration.java
+++ src/servletApplication/ServletAnnotationConfiguration.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-//@WebServlet("/add")
+@WebServlet("/AddServletAnnotation")
 public class ServletAnnotationConfiguration extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
