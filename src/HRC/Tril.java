03395db49174d7a79d34193f868116b88b0cfa4f
Ankan Mukherjee <1828049@kiit.ac.in>
2021-05-22 15:07:22 +0530

    After Hardening Trial

--- src/HRC/Tril.java
+++ src/HRC/Tril.java
@@ -2,15 +2,6 @@ package HRC;
 
 public class Tril {
     public static void main(String[] args) {
-        int a[] = {1,2,3,4,5};
-        printArray(a);
-    }
-
-    private static void printArray(int[] a) {
-
-        for (int i :
-                a) {
-            System.out.println(i);
-        }
+        System.out.println("After Hardening");
     }
 }
