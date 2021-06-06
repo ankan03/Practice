0a7cc252ac2515b3f6de77bde339c7a66379aeab
Ankan Mukherjee <1828049@kiit.ac.in>
2021-06-06 17:58:48 +0530

    Different Annotation used

--- src/com/practice/controller/MainClass.java
+++ src/com/practice/controller/MainClass.java
@@ -1,29 +1,51 @@
 package com.practice.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import com.practice.model.Address;
 import com.practice.model.Student;
 
 
 public class MainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Configuration config = new Configuration().configure("com/practice/cfg/hibernate.cfg.xml");
 		SessionFactory factory = config.buildSessionFactory();
 		Session session = factory.openSession();
 		Transaction txn = null;
 		
+//		Student class object
 		Student student1 = new Student("Ankan","Chinsurah");
 		Student student2 = new Student("Abhishek","Patna");
+		
+		//Reading image
+		FileInputStream fileInputStream1 = new FileInputStream("src/com/practice/model/Screenshot (49).png");
+		byte[] data1 = new byte[fileInputStream1.available()];
+		fileInputStream1.read(data1);
+		
+		FileInputStream fileInputStream2 = new FileInputStream("src/com/practice/model/google.png");
+		byte[] data2 = new byte[fileInputStream2.available()];
+		fileInputStream2.read(data2);
+		
+		//Address object
+		Address address1 = new Address("Chatterjee Bagan", "Chinsurah", true, new Date(), 123.45, data1);
+		Address address2 = new Address("Hospital road", "Akalpoush", false, new Date(), 123.45, data2);
 		
 			try {
 				txn = session.beginTransaction();
 				session.save(student1);
 				session.save(student2);
+				session.save(address1);
+				session.save(address2);
 				txn.commit();
 			} catch (HibernateException e) {
 				if (txn != null)
---
+++ src/com/practice/model/Address.java
@@ -0,0 +1,102 @@
+package com.practice.model;
+
+import java.util.Date;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "address")
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "address_id")
+	private int addressId;
+	
+	@Column(name = "STREET",length = 100)
+	private String street;
+	
+	@Column(name = "CITY",length = 10)
+	private String city;
+	
+	@Column(name = "is_open")
+	private boolean isOpen;
+	
+	@Column(name = "added_date")
+	@Temporal(TemporalType.DATE)
+	private Date date;
+	
+	@Transient
+	private Double x;
+	
+	@Lob
+	private byte[] image;
+	
+	public Address() {
+		super();
+	}
+	public Address(String street, String city, boolean isOpen, Date date, Double x, byte[] image) {
+		super();
+		this.street = street;
+		this.city = city;
+		this.isOpen = isOpen;
+		this.date = date;
+		this.x = x;
+		this.image = image;
+	}
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public boolean isOpen() {
+		return isOpen;
+	}
+	public void setOpen(boolean isOpen) {
+		this.isOpen = isOpen;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Double getX() {
+		return x;
+	}
+	public void setX(Double x) {
+		this.x = x;
+	}
+	public byte[] getImage() {
+		return image;
+	}
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+	
+	
+	
+	
+}
