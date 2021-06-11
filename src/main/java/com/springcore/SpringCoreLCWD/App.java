5a6b1dde8abcbb6d23f70250895235a129ec8455
Ankan Mukherjee <1828049@kiit.ac.in>
2021-06-11 21:28:04 +0530

    Multiple object created through Setter Injection

--- src/main/java/com/springcore/SpringCoreLCWD/App.java
+++ src/main/java/com/springcore/SpringCoreLCWD/App.java
@@ -12,7 +12,9 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        Student student = (Student) context.getBean("studentbean");
-        System.out.println(student);
+        Student student1 = (Student) context.getBean("studentbean");
+        Student student2 = (Student) context.getBean("studentbean1");
+        System.out.println(student1);
+        System.out.println(student2);
     }
 }
