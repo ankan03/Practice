1605040ed4f7ca830e611bd3dafc841bd0a42d64
Ankan Mukherjee <1828049@kiit.ac.in>
2021-06-12 07:19:54 +0530

    Injecting Reference Type in Setter Injection

---
+++ src/main/java/com/springcore/ref/A.java
@@ -0,0 +1,38 @@
+package com.springcore.ref;
+
+public class A {
+	private int id;
+	private B obj;
+	
+	public A() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public B getObj() {
+		return obj;
+	}
+
+	public void setObj(B obj) {
+		this.obj = obj;
+	}
+
+	@Override
+	public String toString() {
+		return "A [id=" + id + ", obj=" + obj + "]";
+	}
+
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/springcore/ref/B.java
@@ -0,0 +1,27 @@
+package com.springcore.ref;
+
+public class B {
+	private int id;
+
+	public B() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "B [id=" + id + "]";
+	}
+
+	
+	
+	
+}
