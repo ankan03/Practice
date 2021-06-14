99acb677586dbbfc23450f009dbe9a485854f59f
Ankan Mukherjee <1828049@kiit.ac.in>
2021-06-14 23:57:08 +0530

    Struts project initial setup

---
+++ src/com/struts2/introduction/Action.java
@@ -0,0 +1,9 @@
+package com.struts2.introduction;
+
+public class Action {
+
+	public String execute() {
+		System.out.println("Inside execute()  method of Action Class");
+		return "success";
+	}
+}
