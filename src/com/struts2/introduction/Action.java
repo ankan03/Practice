f06b38f90dc2c42d8d771ff524a5d7f8565a52cc
Ankan Mukherjee <1828049@kiit.ac.in>
2021-06-15 09:38:10 +0530

    Accessing Input Parameters from URL

--- src/com/struts2/introduction/Action.java
+++ src/com/struts2/introduction/Action.java
@@ -2,9 +2,11 @@ package com.struts2.introduction;
 
 public class Action {
 	private String isStudent;
+	private String subject;
+	
 	public String execute() {
 		System.out.println("Inside execute()  method of Action Class");
-		isStudent = new Student().show();
+		isStudent = new Student().show(getSubject());
 		return "success";
 	}
 	public String getIsStudent() {
@@ -14,6 +16,12 @@ public class Action {
 	public void setIsStudent(String isStudent) {
 		System.out.println("Inside setIsStudent() method of Action class");
 		this.isStudent = isStudent;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
 	}
 	
 	
--- src/com/struts2/introduction/Student.java
+++ src/com/struts2/introduction/Student.java
@@ -1,8 +1,13 @@
 package com.struts2.introduction;
 
 public class Student {
-	public String show() {
+	public String show(String subject) {
 		System.out.println("Inside show() method of Student Class");
-		return "I am a Student";
+		// To put the subject the URL will be http://localhost:8081/Struts2Tutorial/demo/getTutorial.action?subject=dsa
+		if (subject.equals("dsa")) {
+			return "Handle with care";
+		} else {
+			return "Don't worry";
+		}
 	}
 }
